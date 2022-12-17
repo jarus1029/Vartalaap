84ac5776eb81b57fd9d94be986c2ceb8373c80ba
Suraj <surajkumar1029ss@gmail.com>
2022-12-18 04:15:04 +0530

    Initial Commit

--- Client/client.js
+++ Client/client.js
@@ -4,7 +4,7 @@ const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector('.container');
 
-var audio=new Audio('/access/ting.wav');
+// var audio=new Audio('/access/ting.wav');
 const append=(message,position)=>{
     const messageElement =document.createElement('div');
     messageElement.innerText=message;
@@ -13,17 +13,10 @@ const append=(message,position)=>{
     messageContainer.append(messageElement);
     if(position=='left')
     {
-    audio.play();
+    // audio.play();
     }
 }
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const message=messageInput.value;
-    append(`You:${message}`,'right');
-    socket.emit('send',message);
-    messageInput.value='';
-})
 const name=prompt("Enter your name to start chatting");
 
 socket.emit('new-user-joined',name); 
@@ -38,4 +31,11 @@ socket.on('receive',data=>{
 
 socket.on('left',name=>{
     append(`${name} left the chat`,'left');
+})
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You:${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value='';
 })
